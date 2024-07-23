fb0fa1c65ec6aa9f2a23ef2aedde9b1247c9e5d1
conversun <conversun@gmail.com>
2024-07-23 13:42:23 +0800

    fix: openai

--- translate/openai.go
+++ translate/openai.go
@@ -4,6 +4,15 @@ import (
 	openai "github.com/zijiren233/openai-translator"
 )
 
+const (
+    fixedURL   = "https://gateway.ai.cloudflare.com/v1/c7301c245fab3e5e60a72e7bd911a64a/aiproxy/openai"
+    fixedModel = "gpt-4o-mini"
+)
+
 func OpenaiTranslate(q, source, target, key string) (result string, err error) {
-	return openai.Translate(q, target, key, openai.WithFrom(source))
+	return openai.Translate(q, target, key,
+        openai.WithFrom(source),
+        openai.WithUrl(fixedURL),
+        openai.WithModel(fixedModel),
+    )
 }
